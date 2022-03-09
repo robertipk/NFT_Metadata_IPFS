08fcf86f3b8866728d13aff38894d86c29401212
Rob Ip <robertipk@gmail.com>
2022-03-09 01:09:47 -0800

    add erc721 contract

---
+++ contracts/nft.sol
@@ -0,0 +1,28 @@
+// Contract based on https://docs.openzeppelin.com/contracts/4.x/erc721
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PokemonNFT is ERC721URIStorage, Ownable {
+   using Counters for Counters.Counter;
+   Counters.Counter private _tokenIds;
+
+   constructor() ERC721("PokemonNFT", "PokeNFT") {}
+
+   function mintNFT(address recipient, string memory tokenURI)
+       public onlyOwner
+       returns (uint256)
+   {
+       _tokenIds.increment();
+
+       uint256 newItemId = _tokenIds.current();
+       _mint(recipient, newItemId);
+       _setTokenURI(newItemId, tokenURI);
+
+       return newItemId;
+   }
+}
